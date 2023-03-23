d3b9c9b6a79d18daeff2b5976fb7eda4a9ea0601
lucasrachid <lucasrachid@hotmail.com>
2023-03-23 19:50:48 -0300

    Aula dia 23/03

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  // Tudo no flutter é um widget
+  // Override, deve implementar o método da classe abstrata
+  // Que serve como referência e não pode ser instanciada.
+  @override
+  Widget build(BuildContext buildcontext) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project_flutter/app.dart';
+
+main() {
+  runApp(App());
+}
