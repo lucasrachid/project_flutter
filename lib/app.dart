3d470daf57215ac55ea76b405bea688f31c8b07e
lucasrachid <lucasrachid@hotmail.com>
2023-03-23 20:09:25 -0300

    Atualizando

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:project_flutter/home.dart';
 
 class App extends StatelessWidget {
   // Tudo no flutter é um widget
   // Override, deve implementar o método da classe abstrata
   // Que serve como referência e não pode ser instanciada.
+
   @override
   Widget build(BuildContext buildcontext) {
-    return MaterialApp();
+    // Clicando no construtor, tem acesso a todos os parâmetros
+    // Obrigatórios ou não
+    return MaterialApp(
+      title: "Primeiro App Flutter Rachid",
+      // home, seria a página principal do aplicativo.
+      home: Home(),
+      color: Colors.blue,
+    );
   }
 }
---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Inicial"),
+      ),
+    );
+  }
+}
